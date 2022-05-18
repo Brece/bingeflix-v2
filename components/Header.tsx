0380870946e9f2922446bd653b578ab3ede18eb6
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-05-18 12:55:47 +0200

    adds completed Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,36 @@
+import { SearchIcon, BellIcon } from '@heroicons/react/solid';
+import Link from 'next/link';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
-const Header = () => {
+export default function Header () {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    const handleScroll = () => {
+        if(window.scrollY > 0) {
+            setIsScrolled(true);
+        } else {
+            setIsScrolled(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
+
     return (
-        <header>
+        <header className={`${isScrolled && 'bg-[#141414]'}`}>
             <div className='flex items-center space-x-2 md:space-x-10'>
                 <Image
                     src='/img/logo-monochrome.svg'
                     alt='BingeFlix Logo'
                     width={150}
                     height={100}
+                    layout='raw'
                     className='cursor-pointer object-contain'
                 />
 
@@ -20,11 +42,22 @@ const Header = () => {
                     <li className='headerLink'>My List</li>
                 </ul>
             </div>
-            <div>
 
+            <div className='flex items-center space-x-4 text-sm font-light'>
+                <SearchIcon className='hidden sm:inline w-6 h-6' />
+                <p className='hidden lg:inline'>Kids</p>
+                <BellIcon className='w-6 h-6' />
+                <Link href='/account'>
+                    <Image 
+                        src='/img/user.jpg'
+                        alt='Profile image'
+                        width={24}
+                        height={24}
+                        layout='raw'
+                        className='cursor-pointer rounded'
+                    />
+                </Link>
             </div>
         </header>
     )
 }
-
-export default Header;
